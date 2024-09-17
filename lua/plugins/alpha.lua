cf6ee6c31fb542c6353b0976b5cbd6a8d69d1c61
melal <melal.1@hotmail.com>
2024-09-17 03:04:56 +0300

    added indent and treejs plugins

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,38 +1,38 @@
-return   {
-    "goolord/alpha-nvim",
-    dependencies = { 
-    'echasnovski/mini.icons'
-  },
-       config = function ()
-         local alpha = require'alpha'
-         local dashboard = require'alpha.themes.dashboard'
-         dashboard.section.header.val = {
-                [[                                 ]],
-                [[           ▄ ▄                   ]],
-                [[       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ]],
-                [[       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ]],
-                [[    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ]],
-                [[  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ]],
-                [[  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄]],
-                [[▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █]],
-                [[█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █]],
-                [[    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ]],
-                [[                                 ]],
-                [[                                 ]],
+return {
+	"goolord/alpha-nvim",
+	dependencies = {
+		"echasnovski/mini.icons",
+	},
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+		dashboard.section.header.val = {
+			[[                                 ]],
+			[[           ▄ ▄                   ]],
+			[[       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ]],
+			[[       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ]],
+			[[    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ]],
+			[[  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ]],
+			[[  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄]],
+			[[▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █]],
+			[[█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █]],
+			[[    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ]],
+			[[                                 ]],
+			[[                                 ]],
+		}
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  Last Session", ":SessionManager load_last_session<CR>"),
+			dashboard.button("r", "  Sessions", ":SessionManager<CR>"),
+			dashboard.button("c", "  Config", ":Neotree focus $HOME/.config/nvim<CR>"),
+			dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+		}
 
-         }
-         dashboard.section.buttons.val = {
-             dashboard.button( "e", "  Last Session" , ":SessionManager load_last_session<CR>"),
-             dashboard.button( "r", "  Sessions" , ":SessionManager<CR>"),
-             dashboard.button( "q", "󰅚  Quit NVIM" , ":qa<CR>"),
-         }
-
-         dashboard.config.opts.noautocmd = true
-
-         vim.cmd[[autocmd User AlphaReady echo 'ready']]
-
-         alpha.setup(dashboard.config)
-     end
- }
+		dashboard.config.opts.noautocmd = true
 
 
+vim.cmd([[
+  autocmd User AlphaReady echo 'Hello ' . expand('$USER') . ' ! Welcome to NeoVim!'
+]])
+		alpha.setup(dashboard.config)
+	end,
+}
