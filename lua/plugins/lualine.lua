6d683638c038b82902f2c3c93fa0a0f0749c5cd0
melal <example@example.com>
2025-03-26 02:20:26 +0300

    Lualine
    New style .
    init.lua : forgot to add on prev commit

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,52 +1,53 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("lualine").setup({
 
-      options = {
-        theme = "auto",
-        component_separators = "",
-        section_separators = "",
-        globalstatus = true,
-      },
+			options = {
+				theme = "auto",
+				component_separators = "",
+				section_separators = "",
+				globalstatus = true,
+			},
 
-      sections = {
-        lualine_a = { { "mode", icon = "" } },
-        lualine_b = { { "branch", icon = "" } },
-        lualine_c = {
-          {
+			sections = {
+				lualine_a = { { "mode", icon = "" } },
+				lualine_b = { { "branch", icon = "" } },
+				lualine_c = {
+					{
 
-            "filename",
-            file_status = true,
-            newfile_status = false,
-            path = 0,
+						"filename",
+						file_status = true,
+						newfile_status = false,
+						path = 0,
 
-            symbols = {
-              modified = "[+]",
-              readonly = "[!]",
-              unnamed = "[󰕎]",
-              newfile = "[New]",
-            },
-          },
-        },
-        lualine_x = {
-          -- function()
-          -- 	local encoding = vim.o.fileencoding
-          -- 	if encoding == "" then
-          -- 		return vim.bo.fileformat .. " :: " .. vim.bo.filetype
-          -- 	else
-          -- 		return encoding .. " :: " .. vim.bo.fileformat .. " :: " .. vim.bo.filetype
-          -- 	end
-          -- end,
-          {
-            "filetype",
-            icon = { align = "right" },
-          },
-        },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-    })
-  end,
+						symbols = {
+							modified = "[+]",
+							readonly = "[!]",
+							unnamed = "[󰕎]",
+							newfile = "[New]",
+						},
+					},
+				},
+				lualine_x = {
+					-- function()
+					-- 	local encoding = vim.o.fileencoding
+					-- 	if encoding == "" then
+					-- 		return vim.bo.fileformat .. " :: " .. vim.bo.filetype
+					-- 	else
+					-- 		return encoding .. " :: " .. vim.bo.fileformat .. " :: " .. vim.bo.filetype
+					-- 	end
+					-- end,
+					{
+						"filetype",
+						icon = { align = "right" },
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
