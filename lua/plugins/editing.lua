a4338910ce93763a4f916ab0f374a3ced9617f89
melal <example@example.com>
2025-11-13 00:51:52 +0300

    Added an amazing plugin 'neotab'

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,4 +1,44 @@
 return {
+	--Tabout
+	{
+		"kawre/neotab.nvim",
+		event = "InsertEnter",
+		opts = {
+			tabkey = "<Tab>",
+			reverse_key = "<S-Tab>",
+			act_as_tab = true,
+			behavior = "nested", ---@type ntab.behavior
+			pairs = { ---@type ntab.pair[]
+				{ open = "(", close = ")" },
+				{ open = "[", close = "]" },
+				{ open = "{", close = "}" },
+				{ open = "'", close = "'" },
+				{ open = '"', close = '"' },
+				{ open = "`", close = "`" },
+				{ open = "<", close = ">" },
+			},
+			exclude = {},
+			smart_punctuators = {
+				enabled = true,
+				semicolon = {
+					enabled = true,
+					ft = { "cs", "c", "cpp", "java" },
+				},
+				escape = {
+					enabled = true,
+					triggers = { ---@type table<string, ntab.trigger>
+						[","] = {
+							pairs = {
+								{ open = "'", close = "'" },
+								{ open = '"', close = '"' },
+							},
+							format = "%s ", -- ", "
+						},
+					},
+				},
+			},
+		},
+	},
 	--AutoPair: Mini.Pairs
 	{
 		"nvim-mini/mini.pairs",
