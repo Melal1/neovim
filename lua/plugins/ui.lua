9eca5b058eb8b3be517d238f2d0050bdeea67a5b
melal <melal.1@hotmail.com>
2026-08-09 18:25:05 +0300

    chore(ui): bump twilight dim alpha and exclude oil

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -141,7 +141,7 @@ return {
 		"folke/twilight.nvim",
 		opts = {
 			dimming = {
-				alpha = 0.4, -- amount of dimming
+				alpha = 0.7, -- amount of dimming
 				-- we try to get the foreground from the highlight groups or fallback color
 				color = { "Normal", "#ffffff" },
 				term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
@@ -157,7 +157,7 @@ return {
 				"table",
 				"if_statement",
 			},
-			exclude = {}, -- exclude these filetypes,
+			exclude = {"oil"}, -- exclude these filetypes,
 		},
 		keys = { { "<leader>ltw", ":Twilight<CR>" } },
 	},
